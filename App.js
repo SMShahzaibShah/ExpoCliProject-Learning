2392728e82070ffd86dbb2abdcd345dea09c4543
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-17 17:31:46 +0500

    Making Todo App

--- App.js
+++ App.js
@@ -1,16 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const vars =() =>{
-    console.log("hy")
+  const [getText,setText]= useState('');
+  const[getList, setList]=useState(['item 1','item 2'])
+
+  const addItem=()=>{
+    console.log(getText)
+    setList([...getList, getText]);
+    setText('')
   }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Todo List</Text>
+      <View style={styles.inputContainer}>
+      <TextInput 
+        style={styles.textInput}
+        placeholder="Enter an Item"
+        onChangeText={text=> setText(text)}
+        value={getText}
+        />
+      <Button title="New" onPress={addItem} />
+      </View>  
+      
+      <View>
+        <Text style={{fontSize: 26, color: 'lightgray'}}>{getText}</Text>
+      </View>
+      
+      <ScrollView style={styles.scrollView}>
+        <Text style={{fontSize: 26}}> {getList.map((item) => 
+          <View style={styles.scrollViewItem}>
+        <Text>{item}</Text>
+    </View> )}
+        </Text>
+      </ScrollView>
     </View>
+    
   );
 }
 
@@ -19,6 +45,40 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+   // justifyContent: 'center',
+   paddingTop: 40,
   },
+  inputContainer:{
+    flexDirection: "row",
+    width: '75%',
+    justifyContent: "space-between",
+    alignItems: "flex-end"
+
+  },
+  textInput:{
+    borderColor: "red",
+    //borderWidth: 2,
+    borderBottomWidth: 2,
+    width: '78%',
+    //borderRadius: 50,
+    fontSize: 16,
+    padding: 10,
+  },
+  text:{
+    fontSize: 32,
+    color: 'darkgrey',
+  },
+  scrollViewItem:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor:"grey",
+    alignSelf: "center",
+    padding: 10,
+    margin:5,
+    width: '70%',
+    borderRadius: 10,
+  },
+  scrollView:{
+    width: '100%',
+  }
 });
