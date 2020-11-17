d8af57bc6417177f97b0494320a5ed6ad717b71a
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-17 16:20:52 +0500

    Initial Cli Project

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   const vars =() =>{
-    
+    console.log("hy")
   }
   return (
     <View style={styles.container}>
