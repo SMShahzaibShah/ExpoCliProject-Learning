df58e975afce09e1d054cbc25035b529bf292fbd
Sayad Muhammad Shahzaib <smshahzaibshah@gmail.com>
2020-11-19 21:05:08 +0500

    App Completed

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
 import React , {useState} from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput,Keyboard, TouchableOpacity, View } from 'react-native';
 import CustomButton from "./components/ButtonComponent";
+import {hardcoreTodoItems} from "./constant/DummyData";
 export default function App() {
   const [getText,setText]= useState('');
-  const[getList, setList]=useState([])
+  const[getList, setList]=useState(hardcoreTodoItems);
+  const[editingItem, seteditingItem]=useState(0);
+
+  
 
   const addItem=()=>{
     console.log(getText)
     setList([...getList, {key: Math.random().toString() ,data:getText}]);
     setText('')
+    Keyboard.dismiss();
   }
+  const updateItem=()=>{
+    setList(list => getList.map(item=> 
+      item.key === editingItem ?
+        {key: item.key, data: getText}
+      : item
+      ));
+      setText('')
+      seteditingItem(0)
+      Keyboard.dismiss();
+    }
   const removeItem=(itemKey)=>{
     //var list = getList.filter(item=> item.key != itemKey)
     setList(list => getList.filter(item=> item.key != itemKey));
   }
+  const editItem=(item)=>{
+    setText(item.data)
+    seteditingItem(item.key)
+  }
+
+  const scrolView=(
+    <ScrollView style={styles.scrollView}>
+    {getList.map((item, index) => 
+    <TouchableOpacity 
+    key={item.key}
+    activeOpacity={0.7}
+    onPress={()=>editItem(item)}
+    >
+        <View style={styles.scrollViewItem}
+          key={item.key}>
+          <Text style={styles.scrollViewText}>{index+1}#: {item.data}</Text> 
+          <TouchableOpacity onPress={()=> removeItem(item.key)}>
+            <View style={styles.crossTextContainer} >
+              <Text style={styles.crossScrollViewText}>X</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+    </TouchableOpacity>
+      )}
+  </ScrollView>
+  );
+
+  const emptyScroolView=(
+    <View>
+    <Text style={{fontStyle:"italic", fontSize: 16, color:'grey' }}>TodoList Empty</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Todo List</Text>
@@ -25,37 +73,20 @@ export default function App() {
         onChangeText={text=> setText(text)}
         value={getText}
         />
-        <CustomButton text = "New" color="red"
-          onPressEvent={addItem}
+        <CustomButton 
+        text = {editingItem === 0 ? "Add" : "Update"} 
+        color={editingItem === 0 ? "red" : "green"}
+          onPressEvent={editingItem === 0 ? addItem : updateItem}
+          disabled={getText.length <= 0}
         />
       {//<Button title="New" onPress={addItem} />
       }
       </View>  
-      
       <View>
         <Text style={{fontSize: 26, color: 'lightgray'}}>{getText}</Text>
       </View>
-      
-      <ScrollView style={styles.scrollView}>
-        {getList.map((item) => 
-        <TouchableOpacity 
-        key={item.key}
-        activeOpacity={0.7}
-        
-        >
-            <View style={styles.scrollViewItem}
-              key={item.key}>
-              <Text style={styles.scrollViewText}>Data: {item.data}</Text> 
-              <TouchableOpacity onPress={()=> removeItem(item.key)}>
-                <View style={styles.crossTextContainer} >
-                  <Text style={styles.crossScrollViewText}>X</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-        </TouchableOpacity>
-          )}
-      </ScrollView>
-
+      {getList.length<= 0 ? emptyScroolView : scrolView}  
+    
     </View>
     
   );
---
+++ constant/DummyData.js
@@ -0,0 +1,6 @@
+export const hardcoreTodoItems=[
+    {key: Math.random.toString(), data:"Shopping"},
+    {key: Math.random.toString(), data:"Exercise"},
+    {key: Math.random.toString(), data:"Prepare Lunch"},
+    {key: Math.random.toString(), data:"Do the Assignment"},
+  ]
